3dfab3453af2b817a49a30b625a6289253d9559e
mohammadYavari <mdyavarii@gmail.com>
2024-09-16 19:04:59 +0330

    make danceing light hold button

--- main.js
+++ main.js
@@ -1,47 +1,20 @@
-const plnas = document.querySelectorAll("button");
-let userScore = 0;
-let computerScore = 0;
+let interval = null;
+const holdBtn = document.querySelector("#holdBtn");
 
-function show(value, win) {
-    console.log(value);
-    const h1 = document.createElement("h1");
-    h1.innerText = `${value} / user : ${userScore} , computer : ${computerScore}`;
-    document.body.appendChild(h1);
-    if (win) {
-        h1.style.color = "green";
+const handeler = (tgl) => {
+    if (tgl) {
+        interval = setInterval(() => {
+            const colors = ["red", "green", "blue", "yellow"];
+            const random = () => Math.floor(Math.random() * colors.length);
+            holdBtn.style.background = `linear-gradient(to right, ${
+                colors[random()]
+            }, ${colors[random()]})`;
+        }, 500);
     } else {
-        h1.style.color = "red";
+        holdBtn.style.background = "#ccc";
+        clearInterval(interval);
     }
-    setTimeout(() => {
-        h1.innerText = "";
-    }, 1000);
-}
+};
 
-function computerPlan() {
-    const plans = ["rock", "paper", "scissor"];
-    const index = Math.floor(Math.random() * plans.length);
-    return plans[index];
-}
-
-function gameHandeler(eve) {
-    const user = eve.target.innerText;
-    const computer = computerPlan();
-
-    if (user == computer) {
-        show("you tie with computer ...", true);
-    } else if (
-        (user == "rock" && computer == "scissor") ||
-        (user == "scissor" && computer == "papper") ||
-        (user == "paper" && computer == "rock")
-    ) {
-        show("you beat computer :)", true);
-        userScore++;
-    } else {
-        show("computer beats you :(", false);
-        computerScore++;
-    }
-}
-
-window.addEventListener("load", () => {
-    plnas.forEach((i) => i.addEventListener("click", gameHandeler));
-});
+holdBtn.addEventListener("mousedown", () => handeler(true));
+holdBtn.addEventListener("mouseup", () => handeler(false));
